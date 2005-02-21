bd7c3620d3ca1e89b7f4e8ee9fe7fe16f687fecf
chuyskywalker <chuyskywalker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-02-21 06:28:18 +0000

    fixed bug that stopped plugin from not working when fields werent available. refined method for including JS file - should be cached by browsers now fixed a speeeeeling error.
    git-svn-id: https://plugins.svn.wordpress.org/live-comment-preview/trunk@815 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- live-comment-preview.php
+++ live-comment-preview.php
@@ -4,7 +4,7 @@ Plugin Name: Live Comment Preview
 Plugin URI: http://dev.wp-plugins.org/wiki/LiveCommentPreview
 Description: Activate to supply users with a live comment preview.
 Author: Jeff Minard
-Version: 1.0
+Version: 1.0.1
 Author URI: http://thecodepro.com/
 */ 
 
@@ -17,12 +17,47 @@ $commentFrom_urlID     = 'url';
 
 // You shouldn't need to edit anything else.
 
+if( stristr($_SERVER['REQUEST_URI'], 'commentPreview.js') ) {
+	header('Content-type: text/javascript');
+	?>
+function preview() {
+
+	if(document.getElementById('<?php echo $commentFrom_commentID ?>')) {
+		var cmnt = document.getElementById('<?php echo $commentFrom_commentID ?>').value;
+	} else {
+		return false;
+	}
+
+	if(document.getElementById('<?php echo $commentFrom_authorID ?>')) {
+		var pnme = document.getElementById('<?php echo $commentFrom_authorID ?>').value;
+	}
+	
+	if(document.getElementById('<?php echo $commentFrom_urlID ?>')) {
+		var purl = document.getElementById('<?php echo $commentFrom_urlID ?>').value;
+	}
+	
+	var NewText = cmnt.replace(/\\n\\n/g, '</p><p>');
+	NewText = NewText.replace(/\\n/g, '<br />');
+	
+	if(purl) {
+		var name = '<a href="' + purl + '">' + pnme + '</a> says:';
+	} else if(pnme) {
+		var name = pnme + " says";
+	} else {
+		var name = "You say";
+	}
+	
+	NewText = '<p><strong>Preview:</strong></p><p><em>' + name + ':</em></p><p>' + NewText + '</p>';
+	document.getElementById('commentPreview').innerHTML = NewText;
+}
+<?php die(); }
+
 add_action('comment_form', 'add_lp');
 add_action('wp_head', 'add_lp_js');
 
 function add_lp($post_id) {
-	global $commentFrom_commentID, $commentFrom_authorID, $commentFrom_urlID;
-	echo('<div id="commentPreivew"></div>');
+	global $commentFrom_commentID;
+	echo('<div id="commentPreview"></div>');
 	echo('<script type="text/javascript"><!-- 
 	document.getElementById("' . $commentFrom_commentID . '").onkeyup = function() { preview(); }; 
 	//--></script>');
@@ -30,41 +65,8 @@ function add_lp($post_id) {
 }
 
 function add_lp_js($ret) {
-	global $commentFrom_commentID, $commentFrom_authorID, $commentFrom_urlID;
-
-	echo("<!-- Script header added -->\n");
-	
-echo <<<ENDJS
-<script type="text/javascript">
-<!--
-
-function preview() {
-	var cmnt = document.getElementById('$commentFrom_commentID').value;
-	var pnme = document.getElementById('$commentFrom_authorID').value;
-	var purl = document.getElementById('$commentFrom_urlID').value;
-	
-	var NewText = cmnt.replace(/\\n\\n/g, '</p><p>');
-	NewText = NewText.replace(/\\n/g, '<br />');
-	
-	if(purl) {
-		name = '<a href="' + purl + '">' + pnme + '</a>';
-	} else {
-		name = pnme;
-	}
-	
-	NewText = '<p><strong>Preview:</strong></p><p><em>' + name + ' says:</em></p><p>' + NewText + '</p>';
-	document.getElementById('commentPreivew').innerHTML = NewText;
-}
-//-->
-</script>
-ENDJS;
-
-/* bad code-highlighter fix <? */
-
-	echo $javascript;
-
+	echo('<script src="' . get_settings('home') . '/wp-content/plugins/live-comment-preview.php/commentPreview.js" type="text/javascript"></script>');
 	return $ret;
 }
-
 
 ?>
